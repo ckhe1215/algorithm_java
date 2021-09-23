df45929bfdf323c8396fd3c715e28952fe782611
ckhe1215 <ckhe1215@gmail.com>
2021-09-23 13:01:39 +0900

    initial commit

---
+++ BOJ/main_2475.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class main_2475 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+
+	}
+
+}
---
+++ BOJ/main_2920.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class main_2920 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("main_2920");
+	}
+
+}
