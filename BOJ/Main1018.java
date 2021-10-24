d1b6e25112b9359be1b8b765cf12083f3cdada98
ckhe1215 <ckhe1215@gmail.com>
2021-10-24 21:13:18 +0900

    211024

---
+++ BOJ/Main1018.java
@@ -0,0 +1,68 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1018 {
+
+	static char map[][] = new char[51][51];
+	
+	static final char map1[][] = {
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+	};
+	
+	static final char map2[][] = {
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+			{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+			{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+	};
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		int n = Integer.valueOf(st.nextToken());
+		int m = Integer.valueOf(st.nextToken());
+		for(int i = 0; i < n; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < m; j++) {
+				map[i][j] = line.charAt(j);
+			}
+		}
+		int min = 51;
+		for (int i = 0; i <= n - 8; i++) {
+			for(int j = 0; j <= m - 8; j++) {
+				int cnt = cmp_map(i, j);
+				min = Math.min(min, cnt);
+			}
+		}
+		System.out.print(min);
+	}
+	
+	public static int cmp_map(int x, int y) {
+		int cmp1 = 0;
+		int cmp2 = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (map[x + i][y + j] != map1[i][j])
+					cmp1++;
+				if (map[x + i][y + j] != map2[i][j])
+					cmp2++;
+			}
+		}
+		return (Math.min(cmp1, cmp2));
+	}
+
+}
