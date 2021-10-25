0f874d81bdac274d85ebb8effce4af267eb7bcc3
ckhe1215 <ckhe1215@gmail.com>
2021-10-25 19:14:04 +0900

    211025

---
+++ BOJ/Main10989.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main10989 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int num = Integer.valueOf(br.readLine());
+		int[] arr = new int[num];
+		for (int i = 0; i < num; i++) {
+			arr[i] = Integer.valueOf(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i : arr) {
+			sb.append(i + "\n");
+		}
+		System.out.print(sb);
+	}
+
+}
