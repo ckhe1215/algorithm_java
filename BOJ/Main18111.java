518f7b063a7d942e28bd1e242e1efbc452612242
ckhe1215 <ckhe1215@gmail.com>
2021-11-03 23:34:05 +0900

    211103

---
+++ BOJ/Main18111.java
@@ -0,0 +1,57 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main18111 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.valueOf(st.nextToken());
+		int m = Integer.valueOf(st.nextToken());
+		int b = Integer.valueOf(st.nextToken());
+		int min = Integer.MAX_VALUE;
+		int max = -1;
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = Integer.valueOf(st2.nextToken());
+				if (min > arr[i][j]) min = arr[i][j];
+				if (max < arr[i][j]) max = arr[i][j];
+			}
+		}
+		int time = Integer.MAX_VALUE;
+		int height = 0;
+		for (int a = min; a <= max; a++) {
+			int temp = check(a, b, arr, n, m);
+			if (temp < time) {
+				time = temp;
+				height = a;
+			} else if (temp == time) {
+				if (a > height) height = a;
+			}
+		}
+		System.out.print(time + " " + height);
+	}
+	
+	private static int check(int a, int inv, int[][] arr, int n, int m) {
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (a < arr[i][j]) {
+					temp += (arr[i][j] - a) * 2;
+					inv += arr[i][j] - a;
+				} else if (a > arr[i][j]) {
+					temp += a - arr[i][j];
+					inv -= a - arr[i][j];
+				}
+			}
+		}
+		if (inv < 0)
+			return Integer.MAX_VALUE;
+		return temp;
+	}
+}
---
+++ BOJ/Main1929.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main1929 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int start = sc.nextInt();
+		int end =sc.nextInt();
+		int[] arr = new int[end + 1];
+		for (int i = 0; i <= end; i++) {
+			arr[i] = i;
+		}
+		arr[1] = 0;
+		for (int i = 2; i <= Math.sqrt(end); i++) {
+			if (arr[i] != 0) {
+				for (int j = i * i; j <= end; j++) {
+					if (j % i == 0)
+						arr[j] = 0;
+				}
+			}
+		}
+		for (int i = start; i <= end; i++) {
+			if (arr[i] != 0)
+				sb.append(i + "\n");
+		}
+		System.out.print(sb);
+	}
+
+}
