91fa47476d24596a92b48c5233f116bc669c924c
ckhe1215 <ckhe1215@gmail.com>
2021-11-04 19:18:33 +0900

    211104

--- BOJ/Main2805.java
+++ BOJ/Main2805.java
@@ -13,24 +13,24 @@ public class Main2805 {
 		int m = Integer.valueOf(st.nextToken());
 		int[] arr = new int[n];
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		int min = 0;
-		int max = 0;
+		long min = 0;
+		long max = 0;
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.valueOf(st2.nextToken());
 			if (arr[i] > max)
 				max = arr[i];
 		}
-		while (min < max) {
-			int mid = (min + max) / 2;
+		while (min <= max) {
+			long mid = (min + max) / 2;
 			long sum = 0;
 			for (int i = 0; i < n; i++) {
 				if (arr[i] > mid)
 					sum += arr[i] - mid;
 			}
-			if (sum > m)
+			if (sum >= m)
 				min = mid + 1;
 			else
-				max = mid;
+				max = mid - 1;
 		}
 		System.out.println(max);
 	}
