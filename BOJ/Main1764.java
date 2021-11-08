b6fffcd6812393ebed603f41fb4f001577a2a716
ckhe1215 <ckhe1215@gmail.com>
2021-11-08 14:40:49 +0900

    211108

---
+++ BOJ/Main1764.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main1764 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.valueOf(st.nextToken());
+		int m = Integer.valueOf(st.nextToken());
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			map.put(br.readLine(), 1);
+		}
+		ArrayList<String> list = new ArrayList<>();
+		for (int i = 0; i < m; i++) {
+			String name = br.readLine();
+			if (map.get(name) != null) {
+				list.add(name);
+			}
+		}
+		System.out.println(list.size());
+		Collections.sort(list);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+
+}
