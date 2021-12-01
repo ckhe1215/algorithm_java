cedf8125df95fa3d2195210b03491f7c7ee5e2f2
ckhe1215 <ckhe1215@gmail.com>
2021-12-01 19:10:20 +0900

    211201

---
+++ boj_class3/Main11047.java
@@ -0,0 +1,31 @@
+package boj_class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main11047 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String[] nums = str.split(" ");
+		int n = Integer.parseInt(nums[0]);
+		int k = Integer.parseInt(nums[1]);
+		Integer[] arr = new Integer[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int cnt = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			if (arr[i] > k) continue;
+			int temp = k / arr[i];
+			cnt += temp;
+			k -= temp * arr[i];
+			if (k == 0) break;
+		}
+		System.out.println(cnt);
+	}
+
+}
