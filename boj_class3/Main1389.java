17cf9fee2e3a13af354f7f4f6242d220f9a83d97
ckhe1215 <ckhe1215@gmail.com>
2021-12-13 03:30:53 +0900

    211212 fix

--- boj_class3/Main1389.java
+++ boj_class3/Main1389.java
@@ -11,20 +11,19 @@ public class Main1389 {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[][] map = new int[n + 1][n + 1];
-		int[][] visit = new int[n + 1][n + 1];
 		for (int i = 0; i < m; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			map[a][b] = 1;
 			map[b][a] = 1;
 		}
-		int min = n;
+		int min = Integer.MAX_VALUE;
 		int min_person = 0;
-		for (int i = n; i >= 1; i++) {
+		for (int i = 1; i <= n; i++) {
 			int cnt = 0;
 			for (int j = 1; j <= n; j++) {
 				if (i == j) continue;
-				cnt += bfs(i, j, map, visit); // i부터 j까지 몇번에 걸쳐 가야하는지 세기
+				cnt += bfs(i, j, map, n); // i부터 j까지 몇번에 걸쳐 가야하는지 세기
 			}
 			if (cnt < min) {
 				min = cnt;
@@ -35,31 +34,25 @@ public class Main1389 {
 		sc.close();
 	}
 	
-	static int bfs(int s, int e, int[][] map, int[][] visit) {
-		int cnt = 1;
+	static int bfs(int s, int e, int[][] map, int num) {
+		int[] visit = new int[num + 1];
 		Queue<Integer> q = new LinkedList<>();
 		for (int i = 1; i < map[0].length; i++) {
 			if (map[s][i] == 1) {
-				if (i == e) {
-					return cnt;
-				}
 				q.offer(i);
-				visit[s][i] = 1;
+				visit[i] = 1;
 			}
 		}
 		while (!q.isEmpty()){
 			int n = q.poll();
-			if (n == e) {
-				return cnt;
-			}
+			if (n == e) break;
 			for (int i = 1; i < map[0].length; i++) {
-				if (map[n][i] == 1 && visit[n][i] == 0) {
+				if (map[n][i] == 1 && visit[i] == 0) {
 					q.offer(i);
-					visit[n][i] = 1;
+					visit[i] = visit[n] + 1;
 				}
 			}
-			cnt++;
 		}
-		return cnt;
+		return visit[e];
 	}
 }
